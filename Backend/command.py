2e1c1fd4112aec072941ee42e790286cc90ae698
Zhang Cheng Chuan <chengchuan@Zhangs-MacBook-Air.local>
2023-04-14 02:51:16 +0800

    Rearrange file structure

---
+++ Backend/command.py
@@ -0,0 +1,42 @@
+from typing import Tuple
+from frontend_api import format_output
+from backend_api import change_account, view_account
+from constants import DEPOSIT, WITHDRAW
+
+# @abstractmethod
+class Command:
+    def execute(self) -> None:
+        # Do nothing
+        pass
+
+# Concrete commands
+class Deposit_Command(Command):
+    def __init__(self, amount) -> None:
+        self.amount = amount
+
+    def execute(self) -> Tuple[bool, list]:
+        status = change_account(self.amount, DEPOSIT)
+        return (status, None)
+
+class Withdraw_Command(Command):
+    def __init__(self, amount) -> None:
+        self.amount = amount
+
+    def execute(self) -> Tuple[bool, list]:
+        status = change_account(self.amount, WITHDRAW)
+        return (status, None)
+
+class Print_Command(Command):
+    def __init__(self) -> None:
+        pass
+
+    def execute(self) -> Tuple[bool, list]:
+        status, transactions = view_account()
+        return (status, transactions)
+
+
+
+
+
+
+    
